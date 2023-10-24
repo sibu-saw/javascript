782487e2f3bc25f7e9f0dbf8f98218cffca7fb2b
Subir Saw <Subir.Saw@softwareag.com>
2023-10-24 12:09:10 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let btnRegister = document.getElementById("btnRegister");
+btnRegister.addEventListener("click", () => {
+    let userName = document.getElementById("txtUserName").value;
+    let password = document.getElementById("txtPassword").value;
+
+    let userDto = {
+        userName: userName,
+        password: password
+    };
+
+    /*
+    */
+    console.log(userDto);
+    console.log(JSON.stringify(userDto));
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let uri = 'http://localhost:8080/api/auth/register';
+    fetch(uri, {
+        method: 'POST',
+        body: JSON.stringify(userDto),
+        headers:headers
+    });
+});
